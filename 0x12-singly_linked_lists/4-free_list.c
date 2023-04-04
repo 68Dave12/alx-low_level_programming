90851810cffff946c94b37805f09ae5c2aa29518
68Dave12 <davegir68@gmail.com>
2023-04-04 14:55:11 -0700

    update

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,19 +2,17 @@
 #include "lists.h"
 
 /**
- * free_list - frees a linked list
- * @head: list_t list to be freed
+ * free_list - Free all malloced spaced of a list
+ * @head: Pointer to the start of the list
  */
 void free_list(list_t *head)
 {
- list_t *temp;
-
- while (head)
- {
- temp = head->next;
- free(head->str);
- free(head);
- head = temp;
- }
+	if (head != NULL)
+	{
+		if (head->next != NULL)
+			free_list(head->next);
+		free(head->str);
+		free(head);
+	}
 }
 
