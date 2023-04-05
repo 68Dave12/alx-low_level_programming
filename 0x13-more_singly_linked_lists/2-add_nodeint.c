8e3d8b2b8636cc56c2d104f5c16c8ad29944e120
68Dave12 <davegir68@gmail.com>
2023-04-05 11:32:12 -0700

    update

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,24 +1,22 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * add_nodeint - adds a new node at the beginning of a linked list
- * @head: pointer to the first node in the list
- * @n: data to insert in that new node
+ * add_nodeint - Add a listint_t node to a list
+ * @head: Pointer to the pointer at the start of the list
+ * @n: The value to assign to the nodes integer value
  *
- * Return: pointer to the new node, or NULL if it fails
+ * Return: The address of the new element, NULL if it fails
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
- listint_t *new;
+	listint_t *new;
 
- new = malloc(sizeof(listint_t));
- if (!new)
- return (NULL);
-
- new->n = n;
- new->next = *head;
- *head = new;
-
- return (new);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
 }
-
--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,35 +1,29 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * add_nodeint_end - adds a node at the end of a linked list
- * @head: pointer to the first element in the list
- * @n: data to insert in the new element
+ * add_nodeint_end - Add a new listint_t node to the end of a list
+ * @head: Pointer to the pointer to the start of the list
+ * @n: The number to assign to integer value of the node
  *
- * Return: pointer to the new node, or NULL if it fails
+ * Return: Address of the new node, NULL if it fails
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
- listint_t *new;
- listint_t *temp = *head;
+	listint_t *new;
+	listint_t *current;
 
- new = malloc(sizeof(listint_t));
- if (!new)
- return (NULL);
-
- new->n = n;
- new->next = NULL;
-
- if (*head == NULL)
- {
- *head = new;
- return (new);
- }
-
- while (temp->next)
- temp = temp->next;
-
- temp->next = new;
-
- return (new);
+	current = *head;
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	while (current != NULL && current->next != NULL)
+		current = current->next;
+	if (current != NULL)
+		current->next = new;
+	else
+		*head = new;
+	return (new);
 }
-
